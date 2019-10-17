b5c38daea56f7299510cb3f6717a38ab9074843b
caelunshun <caelum12321@gmail.com>
2019-10-16 18:58:25 -0600

    Add benchmarks for reading

--- benches/basic.rs
+++ benches/basic.rs
@@ -5,13 +5,16 @@ use convent::raw::{RawEventBuffer, Value};
 use criterion::{BatchSize, Criterion};
 use shrev::EventChannel;
 
-fn raw_single_write(c: &mut Criterion) {
-    c.bench_function("raw_single_write", |b| {
+fn convent(c: &mut Criterion) {
+    c.bench_function("convent", |b| {
         b.iter_batched_ref(
             || RawEventBuffer::with_segment_size(256),
             |buf| {
+                let mut reader = buf.new_reader();
                 for _ in 0..512 {
                     buf.write(Value::Single(1i32));
+                    let mut iter = unsafe { buf.read(&mut reader) };
+                    assert_eq!(iter.next().unwrap().single(), &1);
                 }
             },
             BatchSize::SmallInput,
@@ -19,13 +22,16 @@ fn raw_single_write(c: &mut Criterion) {
     });
 }
 
-fn shrev_single_write(c: &mut Criterion) {
-    c.bench_function("shrev_single_write", |b| {
+fn shrev(c: &mut Criterion) {
+    c.bench_function("shrev", |b| {
         b.iter_batched_ref(
             || EventChannel::with_capacity(256),
             |buf| {
+                let mut reader = buf.register_reader();
                 for _ in 0..512 {
                     buf.single_write(1i32);
+                    let mut iter = buf.read(&mut reader);
+                    assert_eq!(iter.next(), Some(&1));
                 }
             },
             BatchSize::SmallInput,
@@ -33,5 +39,5 @@ fn shrev_single_write(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, raw_single_write, shrev_single_write);
+criterion_group!(benches, convent, shrev);
 criterion_main!(benches);
